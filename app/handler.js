28cb4646fd3c21a7aeecf204b323c73436b57a06
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-11-24 10:56:38 +0000

    app: support install using bot token or auth

--- app/handler.js
+++ app/handler.js
@@ -1,18 +1,35 @@
 'use strict';
 
 const { App, ExpressReceiver } = require('@slack/bolt');
-const oauth = require('./lib/oauth');
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-const app = new App({
-  authorize: oauth.auth,
-  receiver: expressReceiver
-});
+const app = process.env.SLACK_BOT_TOKEN ?
+  makeAppWithToken(process.env.SLACK_BOT_TOKEN, expressReceiver)
+  : makeAppWithOauth(expressReceiver);
 
 require('./lib/bot')(app);
-oauth.install(expressReceiver.app, app.client)
+
+const makeAppWithToken = (token, expressReceiver) => {
+  return new App({
+    token: token,
+    receiver: expressReceiver
+  });
+}
+
+const makeAppWithOauth = expressReceiver => {
+  const oauth = require('./lib/oauth');
+
+  const app = new App({
+    authorize: oauth.auth,
+    receiver: expressReceiver
+  });
+
+  oauth.install(expressReceiver.app, app.client);
+
+  return app;
+}
 
 module.exports.app = require('serverless-http')(expressReceiver.app);
