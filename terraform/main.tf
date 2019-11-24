67f3e9f95a881fd349626e01b4a21f272cd41616
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-11-24 11:03:20 +0000

    terraform: ordered environment variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ module "apigateway" {
   }
 
   environment_variables = {
-    "SLACK_SIGNING_SECRET" : local.slack_signing_secret
     "SLACK_BOT_TOKEN" : local.slack_bot_token
     "SLACK_CLIENT_ID" : local.slack_client_id
     "SLACK_CLIENT_SECRET" : local.slack_client_secret
     "SLACK_SCOPES" : var.slack_scopes
+    "SLACK_SIGNING_SECRET" : local.slack_signing_secret
     "TABLE_NAME" : var.dynamodb_table_name
   }
 }
