657e496107fd974bdf08ca62c15029534a879289
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-11-30 18:18:05 +0000

    terraform: don't require values for client ID and secret as they're not needed for single workspace apps

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,8 @@ locals {
   slack_credentials         = jsondecode(data.aws_secretsmanager_secret_version.slack.secret_string)
   slack_signing_secret      = local.slack_credentials["SLACK_SIGNING_SECRET"]
   slack_bot_token           = lookup(local.slack_credentials, "SLACK_BOT_TOKEN", "")
-  slack_client_id           = local.slack_credentials["SLACK_CLIENT_ID"]
-  slack_client_secret       = local.slack_credentials["SLACK_CLIENT_SECRET"]
+  slack_client_id           = lookup(local.slack_credentials, "SLACK_CLIENT_ID", "")
+  slack_client_secret       = lookup(local.slack_credentials, "SLACK_CLIENT_SECRET", "")
 
   single_workspace          = local.slack_bot_token != ""
 }
