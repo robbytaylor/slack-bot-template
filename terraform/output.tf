57f73d6afd99b40b291db9b2fcf50c503a5bc1fd
Rob Taylor <robbytaylor@users.noreply.github.com>
2019-11-30 20:26:14 +0000

    Added output for slack oauth redirect URL

--- terraform/output.tf
+++ terraform/output.tf
@@ -7,5 +7,9 @@ output slack_events_url {
 }
 
 output slack_install_url {
-  value = "${module.apigateway.api_url}/install"
+  value =  local.single_workspace ? "" : "${module.apigateway.api_url}/install"
+}
+
+output slack_oauth_redirect_url {
+  value = local.single_workspace ? "" : "${module.apigateway.api_url}/oauth"
 }
